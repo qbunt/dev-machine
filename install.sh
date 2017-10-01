4bc231de9477f30d7c1fa69ea441f4a3a83b813a
Jeremy Bunting <buntingj@gmail.com>
2017-09-30 21:13:13 -0700

    adding most machine dependencies on personal machine, waiting to add deps on others

--- install.sh
+++ install.sh
@@ -2,15 +2,24 @@
 
 xcode-select --install
 
+DOTFILE_LOCATION=~/Dropbox/dotfiles
+
 # dotfiles
-ln -s ~/Dropbox/dotfiles/.zshrc ~/.zshrc
-ln -s ~/Dropbox/dotfiles/.gitconfig ~/.gitconfig
+ln -s ${DOTFILE_LOCATION}/.zshrc ~/.zshrc
+ln -s ${DOTFILE_LOCATION}/.gitconfig ~/.gitconfig
+
 
 # brew stuff
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-cd ~/Dropbox/dotfiles
+
+cd ${DOTFILE_LOCATION}
 brew bundle
 cd ~
+
+# node
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+
+# copy macOs files
 
 echo "******************** Done ********************"
 echo "Don't forget to configure SSH properly with key and config"
